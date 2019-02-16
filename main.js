040d87eda2375879fb7e62df712a9877e85cf025
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-15 19:42:23 -0500

    set sky rows (14)

--- main.js
+++ main.js
@@ -2,8 +2,11 @@
 var practer = document.getElementsByClassName("practer");
 // var colorArr = ['red', 'green', 'cadetblue', 'black', 'cornflowerblue'];
 var colorX = 'dodgerblue';
-var colorArr2 = [colorX,'lightskyblue',colorX,'lightskyblue', 'rgb(200, 250, 250)', 'lightskyblue', 'rgb(200, 250, 250)', 'lightskyblue', 'lightgoldenrodyellow','lemonchiffon','lightyellow','navajowhite','#fff196','yellow'];
-var skyHeights = ['8', '2', '20', '12', '2', '2', '9', '8', '3', '4', '5', '2', '2'];
+var colorArr2 = [colorX,'lightskyblue',colorX,'lightskyblue', 'rgb(200, 250, 250)', 'lightskyblue', 'rgb(200, 250, 250)',
+'orange', 'rgb(200, 250, 250)',
+'orange', '#fff196', 'lightgoldenrodyellow', '#fff196', 'lightgoldenrodyellow'];
+var skyHeights = ['8', '2', '10', '8', '2', '2', '6',
+'4', '5', '8', '6','4','3','4'];
 
 for (i=0; i<practer.length; i++) {
   console.log(practer[i]);
@@ -22,3 +25,4 @@ for (i=0; i<practer.length; i++) {
 // navajowhite
 // yellow
 // fff196
+// orange

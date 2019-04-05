24afe00045549a4621648017976de40950557231
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-04-05 14:15:56 -0400

    adjust skyline

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ var colorX = 'dodgerblue';
 var colorArr2 = [colorX,'lightskyblue', 'lightskyblue','lightskyblue', 'rgb(200, 250, 250)', 'lightskyblue', 'rgb(200, 250, 250)',
 'orange', 'rgb(200, 250, 250)',
 'orange', '#fff196', 'lightgoldenrodyellow', '#fff196', 'lightgoldenrodyellow'];
-var skyHeights = ['8', '2', '8', '10', '4', '4', '8',
-'4', '4', '8', '6','4','3','20'];  // From top orange
+var skyHeights = ['8', '2', '7', '9', '3', '3', '7',
+'4', '3', '8', '6','4','3','20'];  // From top orange
 
 for (i=0; i<practer.length; i++) {
   console.log(practer[i]);

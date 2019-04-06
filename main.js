1b20741a3c7128a0f6191d8d84cee7649b8d6ee4
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-04-06 00:41:31 -0400

    pine tree assembled

--- main.js
+++ main.js
@@ -9,10 +9,46 @@ var skyHeights = ['8', '15', '3', '3', '7',
 '4', '3', '8', '6','4','3','35'];  // From top orange
 
 for (i=0; i<practer.length; i++) {
-  console.log(practer[i]);
   practer[i].style.background = `${colorArr2[i]}`
   practer[i].style.height = `${skyHeights[i]}vh`
 }
+
+
+// Make triangles in trees different sizes
+var pineTrees = document.getElementsByClassName("pine-tree");
+
+for (var pine in pineTrees) {
+  
+
+
+
+  console.log(pineTrees[pine]);
+
+}
+
+
+// .pine-tree:first-child {
+//   margin-top: 0;
+// }
+
+
+
+
+// pineTrees.map( pineTree => {
+//   console.log(pineTree);
+//
+//   // for (let i = 0; i > pineTree.length; i ++) {
+//   //   console.log(pineTree[i]);
+//   //
+//   // }
+// });
+// console.log(pineTrees);
+// console.log(pineTrees[0]);
+
+
+
+
+
 
 
 // 200,250,250   45,52   rgb(200, 250, 250);

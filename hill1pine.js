cbfeed42a0ca007a6abf6da9ca5a7184758b8b39
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-04-06 11:46:05 -0400

    back treeline  population function aligned

--- hill1pine.js
+++ hill1pine.js
@@ -1,19 +1,28 @@
-// let hill1 = document.getElementById('hill1');
-//
-// function treeline() {
-//   for (let i = 0; i < 90; i ++) {
-//     let xPos = i * 0.8;
-//     let yPos = 6 - (xPos / 7);
-//     let pine = document.createElement('div');
-//     pine.classList.add('small-pine');
-//     pine.style.left = `${xPos}vw`;
-//     pine.style.top = `${yPos}vh`;
-//     pine.style.borderLeft = '0.5vw solid transparent';
-//     pine.style.borderRight = '0.5vw solid transparent';
-//     pine.style.borderBottom = '3vw solid var(--hill1-color)';
-//     hill1.appendChild(pine);
-//   }
-// }
-//
-//
-// treeline();
+let hill1 = document.getElementById('hill1');
+let pineTree = document.getElementsByClassName('pine-tree')[0];
+
+
+
+function hill1Treeline() {
+  let xPosMod = 0;
+  let yPosMod = 0;
+
+  for (let i = 0; i < 30; i ++) {
+    let pineCopy = pineTree.cloneNode(true);
+    let newDiv = document.createElement('div');
+    newDiv.appendChild(pineCopy)
+
+    newDiv.style.position = 'absolute';
+    newDiv.style.display = 'inline-block';
+    newDiv.style.left = `${xPosMod + 64}%`;
+    newDiv.style.marginTop = `${-1.3 - yPosMod}%`;
+    hill1.appendChild(newDiv);
+
+    xPosMod += 1 + Math.random();
+    yPosMod += 0.04;
+  }
+
+}
+
+
+hill1Treeline();

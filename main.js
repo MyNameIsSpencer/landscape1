27ad54e04354caeff9aca7a218d623fc4f2e9146
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-04-06 12:04:26 -0400

    top nav height to 8vh, coincide with skyline

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
-var practer = document.getElementsByClassName("practer");
-// var colorArr = ['red', 'green', 'cadetblue', 'black', 'cornflowerblue'];
+var skyline = document.getElementsByClassName("skyline");
 var colorX = 'dodgerblue';
 var colorArr2 = [colorX,'lightskyblue', 'rgb(200, 250, 250)', 'lightskyblue', 'rgb(200, 250, 250)',
 'orange', 'rgb(200, 250, 250)',
@@ -8,9 +7,9 @@ var colorArr2 = [colorX,'lightskyblue', 'rgb(200, 250, 250)', 'lightskyblue', 'r
 var skyHeights = ['8', '15', '3', '3', '7',
 '4', '3', '8', '6','4','3','35'];  // From top orange
 
-for (i=0; i<practer.length; i++) {
-  practer[i].style.background = `${colorArr2[i]}`
-  practer[i].style.height = `${skyHeights[i]}vh`
+for (i=0; i<skyline.length; i++) {
+  skyline[i].style.background = `${colorArr2[i]}`
+  skyline[i].style.height = `${skyHeights[i]}vh`
 }
 
 
@@ -18,48 +17,10 @@ for (i=0; i<practer.length; i++) {
 var pineTrees = document.getElementsByClassName("pine-tree");
 
 for (var pine in pineTrees) {
-  
+
 
 
 
   console.log(pineTrees[pine]);
 
 }
-
-
-// .pine-tree:first-child {
-//   margin-top: 0;
-// }
-
-
-
-
-// pineTrees.map( pineTree => {
-//   console.log(pineTree);
-//
-//   // for (let i = 0; i > pineTree.length; i ++) {
-//   //   console.log(pineTree[i]);
-//   //
-//   // }
-// });
-// console.log(pineTrees);
-// console.log(pineTrees[0]);
-
-
-
-
-
-
-
-// 200,250,250   45,52   rgb(200, 250, 250);
-// 9687eb
-// skyblue
-// royalblue
-// powderblue
-// lightgoldenrodyellow
-// lemonchiffon
-// lightyellow
-// navajowhite
-// yellow
-// fff196
-// orange

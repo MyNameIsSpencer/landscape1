61c2ff893530b7d095e780cbc2ebda71e17b3e33
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-04-07 12:27:01 -0400

    reduce final skyline height

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var colorArr2 = [colorX,'lightskyblue', 'rgb(200, 250, 250)', 'lightskyblue', 'r
 'orange', 'rgb(200, 250, 250)',
 'orange', '#fff196', 'lightgoldenrodyellow', '#fff196', 'lightgoldenrodyellow'];
 var skyHeights = ['8', '15', '3', '3', '7',
-'4', '3', '8', '6','4','3','35'];  // From top orange
+'4', '3', '8', '6','4','3','15'];  // From top orange
 
 for (i=0; i<skyline.length; i++) {
   skyline[i].style.background = `${colorArr2[i]}`
